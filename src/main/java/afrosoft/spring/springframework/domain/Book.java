978f3aac3be83cd4786e28b50c0dc35536d2e7b1
tyfahpundo <tafadzwaallentyfah@gmail.com>
2021-08-24 09:29:51 +0200

    Equality in Hibernate

--- src/main/java/afrosoft/spring/springframework/domain/Book.java
+++ src/main/java/afrosoft/spring/springframework/domain/Book.java
@@ -13,7 +13,8 @@ public class Book {
     private String title;
     private String isbn;
     @ManyToMany
-    @JoinTable(name="author_book", joinColumns = @JoinColumn(name="book_id"),inverseJoinColumns = @JoinColumn(name="author_id"))
+    @JoinTable(name="author_book", joinColumns = @JoinColumn(name="book_id"),
+            inverseJoinColumns = @JoinColumn(name="author_id"))
     private Set<Author> authors;
 
     public Book() {
@@ -55,5 +56,30 @@ public class Book {
 
     public void setAuthors(Set<Author> authors) {
         this.authors = authors;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        return id != null ? id.equals(book.id) : book.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", authors=" + authors +
+                '}';
     }
 }
